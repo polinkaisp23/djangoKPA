aaa4ecc7157b057264d611ece4c61d7c89d34146
kabinet303 <kabinet303@localhost.localdomain>
2026-01-28 13:39:05 +0300

    register

--- korulevaPA/mainKPA/views.py
+++ korulevaPA/mainKPA/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
+
+def RegFunc(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()      # сохраняем в БД
+            login(request, user)    # автологин
+            return redirect('/main/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required(login_url='/register/')
 def mainFunc(request):
-    #return HttpResponse('Главная функция главной страницы главного блока')
     return render(request, 'main.html')
